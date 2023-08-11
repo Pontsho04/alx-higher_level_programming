96f66ad8929d3dd2c4727fe3c02c5a09c84ed274
Pontsho04 <mabopontsho@gmail.com>
2023-08-11 13:18:06 -0700

    updated

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/in/python3
+
+if __name__ == "__main__":
+    """Print all the names defined by a hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
