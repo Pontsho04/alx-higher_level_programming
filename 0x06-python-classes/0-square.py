b7096b0a2f36d261f142ff59033ebc86e7ffa06a
Pontsho04 <Pontsho04>
2023-08-29 12:28:13 -0700

    0x06 python

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+
 """Define a class Square."""
 
+
 class Square:
-     """Represent a square."""
-      pass
+    def __init__(self, side_length):
+        self.side_length = side_length
+
+    def area(self):
+        return self.side_length ** 2
