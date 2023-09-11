8008b21c7ddd89e901d5d7fe1fa0645bc4d06545
Pontsho04 <Pontsho04>
2023-09-11 16:05:29 -0700

    0x0a python

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-Square = __import__('11-square').Square
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-s = Square(13)
+class Square(Rectangle):
+    """Represent a square."""
 
-print(s)
-print(s.area())
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
